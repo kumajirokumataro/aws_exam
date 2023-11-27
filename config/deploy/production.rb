5566c3d47837976a92f28f266034885deb3b3153
はらなおみ <haraweb7@gmail.com>
2023-11-27 16:07:09 +0900

    aws

---
+++ config/deploy/production.rb
@@ -0,0 +1,2 @@
+server '35.73.154.131', user: 'app', roles: %w{app db web}
+set :ssh_options, keys: '/Users/naomihara/.ssh/id_rsa'
--- db/migrate/20231127051814_create_blogs.rb
+++ db/migrate/20231127051814_create_blogs.rb
@@ -8,3 +8,4 @@ class CreateBlogs < ActiveRecord::Migration[6.1]
     end
   end
 end
+
